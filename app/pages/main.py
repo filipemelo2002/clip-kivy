ab97824ac48c7801b2933c4ee3109892b01bc3cd
Filipe Melo <filipe.melo@liferay.com>
2022-10-08 11:49:44 -0300

    feat: remove unnecessary global path instance

--- app/pages/main.py
+++ app/pages/main.py
@@ -5,12 +5,6 @@ from kivy.app import App
 from kivy.lang.builder import Builder
 from CreateGroup import CreateGroup
 
-import os.path
-from kivy.resources import resource_add_path
-KV_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), 'ui'))
-
-resource_add_path(KV_PATH)
-
 class Application(App):
     def build(self):
         return CreateGroup.CreateGroup()
